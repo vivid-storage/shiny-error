f810e7cc0d73200294758c95c0931ed63dc0b9c0
vivid-storage <165187161+vivid-storage@users.noreply.github.com>
2024-05-02 09:59:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import random
 
 ui.page_opts(title="Penguins dashboard", fillable=True)
 
-webUrl = request.urlopen(f"gist.githubusercontent.com/vivid-storage/8c3c5ffc83e847f34e46aa77a21327f4/raw?cachebust={random.randint(1,100000)}")
+webUrl = request.urlopen(f"https://gist.githubusercontent.com/vivid-storage/8c3c5ffc83e847f34e46aa77a21327f4/raw?cachebust={random.randint(1,100000)}")
 data = webUrl.read().decode("utf-8")
 
 print(f"Success gist: {data}")
